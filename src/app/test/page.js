f8950899b6344a16006e08c0d02e49b9eb372c77
Hossein rashidi <hossein.rsh60131@gmail.com>
2023-08-06 22:44:13 +0430

    test pushing

---
+++ src/app/test/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+const page = () => {
+  return (
+    <div>
+      <h2> ine test</h2>
+      <Link href="/testing">تست</Link>
+    </div>
+  )
+}
+
+export default page
